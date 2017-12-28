b2c220c6f19f3b84fbd11614b95ce49b3b6d99d1
Nicolas Kagami <nicolaskagami@gmail.com>
2017-12-28 09:16:28 -0200

    Calculating Dispersion

--- sink/capest.c
+++ sink/capest.c
@@ -9,16 +9,23 @@
 #include <arpa/inet.h>
 
 #define BUFSIZE 1024
+#define ESTIMATE_SAMPLE_AMOUNT 20480
 
 uint32_t last_s1_et =0;
 uint32_t last_s1_it =0;
 uint32_t last_s2_it=0;
 uint32_t last_s2_et=0;
 uint32_t last_length = 0;
-float last_estimates[100];
+float last_estimates[ESTIMATE_SAMPLE_AMOUNT];
+float ordered_estimates[ESTIMATE_SAMPLE_AMOUNT];
 uint32_t estimate_iterator = 0;
+uint32_t notfirst = 0;
 
 void submit(uint32_t ip_length, uint32_t swid_source, uint32_t swid_target, uint32_t source_in_timestamp, uint32_t target_in_timestamp);
+int compare (const void * a, const void * b)
+{
+  return ( *(uint32_t*)a - *(uint32_t*)b );
+}
 
 int main(int argc, char **argv) 
 {
@@ -91,9 +98,9 @@ int main(int argc, char **argv)
             et = ntohl(*(uint32_t *)&(buf_aux[8]));
             //printf("Swid: %d, Ingress Time: %u, Enqueue Time: %u\n",swid,it,et);
             buf_aux+=12;
-            if(swid == 1)
+            if(i == 1)
                 current_s1_it = it;
-            if(swid == 2)
+            if(i == 0)
                 current_s2_it = it;
          }
 
@@ -106,22 +113,53 @@ void submit(uint32_t ip_length,uint32_t swid_source, uint32_t swid_target, uint3
 {  
     float delta_S1_it = (float) (source_in_timestamp- last_s1_it);   
     float delta_S2_it = (float) (target_in_timestamp- last_s2_it);   
+    //if(last_s1_it && (delta_S1_it>delta_S2_it))
+    //{
+    //    return;
+    //}
     float estimate = (8000*last_length)/delta_S2_it;
-    last_estimates[estimate_iterator++%100] = estimate;
+    last_estimates[estimate_iterator++%ESTIMATE_SAMPLE_AMOUNT] = estimate;
     float average,sum;
     sum = 0;
     uint32_t i,limit;
-    limit = (estimate_iterator>100) ? 100 : estimate_iterator;
+    limit = (estimate_iterator>ESTIMATE_SAMPLE_AMOUNT) ? ESTIMATE_SAMPLE_AMOUNT : estimate_iterator;
     for(i=0;i<limit;i++)
         sum+=last_estimates[i];
     average = sum/limit;
+    for(i=0;i<limit;i++)
+        ordered_estimates[i] = last_estimates[i];
+    qsort(ordered_estimates,limit,sizeof(uint32_t),compare);
+    average = ordered_estimates[(uint32_t)(limit/2)];
 
+    uint32_t highest_count = 0;
+    uint32_t highest_avg = 0;
+    if(limit< 100)
+        return;
+    for(i=50;i<limit-50;i++)
+    {
+        int j;
+        int count =0;
+        uint32_t median = ordered_estimates[i];
+        for(j=-50;j<50;j++)
+        {
+            if((ordered_estimates[i+j]>(0.9*median))&&(ordered_estimates[i+j]<(1.1*median)))
+                count++;
+        }
+        if(count>highest_count)
+        {
+            highest_count = count;
+            highest_avg = median; 
+        }
+    }
+    average = highest_avg;
+
+    printf("Count: %d\n",highest_count);
     printf("SRC IT dispersion estimate: %f Kbps\n",(float)(8000*last_length)/delta_S1_it);
     printf("TGT IT dispersion estimate: %f Kbps\n",estimate);
     printf("Average dispersion estimate (%u): %f Kbps\n",limit,average);
 
-    printf("SRC IT dispersion: %f ms\n",(float)delta_S1_it/(1000));
-    printf("TGT IT dispersion: %f ms\n",(float)delta_S2_it/(1000));
+    //printf("SRC IT dispersion: %f ms\n",(float)delta_S1_it/(1000));
+    //printf("TGT IT dispersion: %f ms\n",(float)delta_S2_it/(1000));
     last_length = ip_length;
     last_s1_it = source_in_timestamp;
     last_s2_it = target_in_timestamp;
