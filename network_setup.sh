02bc0b1a87fe8d84e4eb6636de969534392d3e57
Nicolas Kagami <nicolaskagami@gmail.com>
2018-01-10 09:31:14 -0800

    Network setup script

---
+++ network_setup.sh
@@ -0,0 +1,6 @@
+for INTERFACE in s1-eth1 s1-eth2 s1-eth3 s2-eth1 s2-eth2 s2-eth3 s3-eth1 s3-eth2 s3-eth3;
+do
+    tc qdisc del dev $INTERFACE root
+    ifconfig $INTERFACE mtu 9000 up
+    tc qdisc add dev $INTERFACE root tbf rate 4Mbit latency 10ms burst 9000
+done
