eed6896991225b8bd8073cb43e0aef89119043c3
root <root@nsk-mininet.inf.ufrgs.br>
2018-01-29 16:56:02 -0800

    Video testing environment + autocorrelation

--- network_setup.sh
+++ network_setup.sh
@@ -2,5 +2,5 @@ for INTERFACE in s1-eth1 s1-eth2 s1-eth3 s2-eth1 s2-eth2 s2-eth3 s3-eth1 s3-eth2
 do
     tc qdisc del dev $INTERFACE root
     ifconfig $INTERFACE mtu 9000 up
-    tc qdisc add dev $INTERFACE root tbf rate 4Mbit latency 10ms burst 9000
+    tc qdisc add dev $INTERFACE root tbf rate 10Mbit latency 10ms burst 9000
 done
